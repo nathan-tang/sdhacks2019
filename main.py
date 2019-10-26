4f69620cb1297d4fa6deb625b39ac4853909b086
Michael Ren <renm2@uci.edu>
2019-10-25 23:38:04 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import boto3
+import credentials
 
+
+         
 def detect_labels_local_file(photo):
 
-
-    client=boto3.client('rekognition', region_name='us-west-2')
+    client=boto3.client('rekognition', region_name='us-west-2', aws_access_key_id=credentials.access_key,
+         aws_secret_access_key=credentials.secret_key)
    
     with open(photo, 'rb') as image:
         response = client.detect_labels(Image={'Bytes': image.read()})
